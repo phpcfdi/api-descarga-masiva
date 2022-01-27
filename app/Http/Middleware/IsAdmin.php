3834e3800d48d534aff58da7a0ad9d6573bce8e6
celli33 <limon.martinez.misael@gmail.com>
2022-01-26 23:10:28 -0600

    refactor: remove useless if validation

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Http\Middleware;
 
-use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 
@@ -12,9 +11,8 @@ class IsAdmin
 {
     public function handle(Request $request, Closure $next): mixed
     {
-        /** @var User|null $user */
         $user = $request->user();
-        if ($user && ! $user->is_admin) {
+        if (! $user->is_admin) {
             return response(null, 403);
         }
         return $next($request);
